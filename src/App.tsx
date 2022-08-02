c1a21fa79e49409378532bf6a3a06f2f5e735e2b
Mardeson Pereira <maxpb777@gmail.com>
2022-08-02 13:52:20 -0300

    feat: create default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <h1>hello Wold</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>hello Wold</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
